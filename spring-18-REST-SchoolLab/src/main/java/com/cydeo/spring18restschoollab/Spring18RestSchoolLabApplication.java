9a77ccecfae198ecb260374c092cbd0aad75e69e
GLÊNIO FARIA <glenio.faria@gmail.com>
2022-11-29 15:11:52 -0500

    18-REST-SchoolLab

---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/spring18restschoollab/Spring18RestSchoolLabApplication.java
@@ -0,0 +1,26 @@
+package com.cydeo.spring18restschoollab;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Spring18RestSchoolLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring18RestSchoolLabApplication.class, args);
+    }
+
+
+
+
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+}
