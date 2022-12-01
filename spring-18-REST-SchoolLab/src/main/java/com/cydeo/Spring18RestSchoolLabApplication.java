ecb394dd283cf767085dd2c7f5e271f216aa124f
GLÊNIO FARIA <glenio.faria@gmail.com>
2022-12-01 14:45:47 -0500

    19-REST-OpenAPI3

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/Spring18RestSchoolLabApplication.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/Spring18RestSchoolLabApplication.java
@@ -1,11 +1,10 @@
-package com.cydeo.spring18restschoollab;
+package com.cydeo;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
